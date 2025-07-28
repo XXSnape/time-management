f65c4bb455a6e7a429ec88b08b5b35c39a911785
Snape <email>
2025-07-28 20:18:23 +0300

    Write token logic

---
+++ backend/core/dependencies/auth.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt import InvalidTokenError
+from services import auth
+
+http_bearer = HTTPBearer()
+
+
+def get_token_payload(
+    credentials: Annotated[
+        HTTPAuthorizationCredentials,
+        Depends(http_bearer),
+    ],
+) -> dict[str, str | int]:
+    """
+    Декодирует токен и возвращает полезную нагрузку из него
+    :param credentials: Annotated[HTTPAuthorizationCredentials, Depends(http_bearer)]
+    :return: полезная нагрузка в токене.
+    """
+    try:
+        payload = auth.decode_jwt(
+            token=credentials.credentials,
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invalid token error",
+        )
+    return payload
+
+
+def get_user_id(
+    payload: Annotated[dict, Depends(get_token_payload)],
+) -> int:
+    """
+    Получает id пользователя
+    :param payload: Annotated[dict, Depends(get_token_payload)])
+    :return: id пользователя
+    """
+    return int(payload.get("sub"))
---
+++ backend/services/auth.py
@@ -0,0 +1,95 @@
+from datetime import UTC, datetime, timedelta
+
+import bcrypt
+import jwt
+from core.config import settings
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settings.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+    expire_days: int = settings.auth_jwt.access_token_expire_minutes,
+) -> str:
+    """
+    Кодирует jwt-токен
+    :param payload: полезная нагрузка в токене
+    :param private_key: закрытый ключ
+    :param algorithm: алгоритм шифрования
+    :param expire_days: действие токена в днях
+    :return: токен доступа
+    """
+    to_encode = payload.copy()
+    now = datetime.now(UTC)
+    expire = now + timedelta(days=expire_days)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
+    encoded = jwt.encode(
+        to_encode,
+        private_key,
+        algorithm=algorithm,
+    )
+    return encoded
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
+    """
+    Декодирует jwt-токен
+    :param token: токен доступа
+    :param public_key: публичный ключ
+    :param algorithm: алгоритм шифрования
+    :return: данные токена
+    """
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
+
+
+def hash_password(
+    password: str,
+) -> bytes:
+    """
+    Хэширует пароль
+    :param password: пароль
+    :return: хэшированный пароль
+    """
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    """
+    Сравнивает пароли
+    :param password: пароль
+    :param hashed_password: хэшированный пароль
+    :return: результат сравнения
+    """
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
+
+
+def get_access_token(user_id: int) -> str:
+    """
+    Создает токен доступа с полезной нагрузкой в виде id пользователя,
+    которому принадлежит токен
+    :param user_id: id пользователя
+    :return: токен дсотупа
+    """
+    payload = {"sub": str(user_id)}
+    token = encode_jwt(payload=payload)
+    return token
