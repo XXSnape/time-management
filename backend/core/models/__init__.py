6e1e6498d31ae748c1571b66283cf32d124ac57a
Snape <email>
2025-07-28 21:41:57 +0300

    Create task model

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,2 +1,3 @@
 from .base import Base as Base
 from .user import User as User
+from .task import Task as Task
---
+++ backend/core/models/task.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+from sqlalchemy import CheckConstraint, TEXT
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Task(Base):
+    __table_args__ = (
+        CheckConstraint(
+            "deadline_time >= 0 AND deadline_time <= 23",
+            name="ck_time_range",
+        ),
+        CheckConstraint(
+            "hour_before_reminder >= 1 AND hour_before_reminder <= 23",
+            name="ck_reminder_range",
+        ),
+    )
+    name: Mapped[str]
+    description: Mapped[str] = mapped_column(TEXT)
+    deadline_date: Mapped[date]
+    deadline_time: Mapped[int]
+    hour_before_reminder: Mapped[int]
