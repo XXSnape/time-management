e488857dc05cfea2315e7a3d7f7a1b671b0780d5
Snape <email>
2025-07-28 18:57:42 +0300

    Add db helper

---
+++ backend/core/dependencies/helper.py
@@ -0,0 +1,74 @@
+"""
+Модуль для работы с базой данных.
+"""
+
+import logging
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+log = logging.getLogger(__name__)
+
+
+class DBHelper:
+    """
+    Класс - помощник для работы с базой данных.
+    """
+
+    def __init__(self, url: str, echo: bool = False) -> None:
+        """
+        Инициализация класса.
+
+        Параметры:
+        url: Строка для подключения к базе данных
+        echo: Принимает значения True или False
+
+        Если установлен в True, то в консоль будут
+        выводиться запросы к базе. По умолчанию False.
+        """
+        self.engine = create_async_engine(
+            url=url, echo=echo
+        )  # Двигатель для работы с асинхронной базой данных
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autocommit=False,
+            expire_on_commit=False,
+        )  # Фабрика сессий для работы с асинхронной базой данных
+
+    async def dispose(self) -> None:
+        """
+        Освобождает ресурсы, связанные с базой данных.
+        """
+        await self.engine.dispose()
+        log.info("Соединение с базой данных закрыто.")
+
+    async def get_async_session_without_commit(
+        self,
+    ) -> AsyncGenerator[AsyncSession, None]:
+        """
+        Возвращает сессию для асинхронной работы с базой данных.
+        """
+        async with self.session_factory() as session:  # type: AsyncSession
+            yield session
+
+    async def get_async_session_with_commit(
+        self,
+    ) -> AsyncGenerator[AsyncSession, None]:
+        """
+        Возвращает сессию для асинхронной работы с базой данных и делает коммит.
+        """
+        async with self.session_factory() as session:  # type: AsyncSession
+            yield session
+            await session.commit()
+
+
+db_helper = DBHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
