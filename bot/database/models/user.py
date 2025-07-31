b16d96349ac6c06b596a46a623d4205bd8fe90c6
Snape <email>
2025-07-31 14:12:43 +0300

    Add language and index on telegram id

--- bot/database/models/user.py
+++ bot/database/models/user.py
@@ -2,8 +2,16 @@ from sqlalchemy import BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from database.utils.enums import Languages
 
 
 class User(Base):
-    telegram_id: Mapped[int] = mapped_column(BigInteger)
+    telegram_id: Mapped[int] = mapped_column(
+        BigInteger,
+        index=True,
+        unique=True,
+    )
     access_token: Mapped[str]
+    language: Mapped[Languages] = mapped_column(
+        default=Languages.ru, server_default=Languages.ru
+    )
