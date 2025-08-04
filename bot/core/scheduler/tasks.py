92b6d2437a729189a1c214dc873f2686904ebb93
Snape <email>
2025-08-04 19:22:40 +0300

    Send message to habit queue

--- bot/core/scheduler/tasks.py
+++ bot/core/scheduler/tasks.py
@@ -2,4 +2,5 @@ from core.config import broker
 
 
 async def remind_about_tasks_and_habits():
-    await broker.publish(queue="reminder")
+    await broker.publish(queue="remind_about_tasks")
+    await broker.publish(queue="remind_about_habits")
